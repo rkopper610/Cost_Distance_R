0ed2cd8501dcf11e7dc39fc60f8f356031782452
rkopper610 <rkopper@clarku.edu>
2019-08-06 14:00:08 -0400

    cropping and working with gdistance examples

---
+++ cropping_CD_.R
@@ -0,0 +1,59 @@
+library(gdistance)
+library(raster)
+
+#LOAD in data
+r1 <- "C:/Users/rkopp/Desktop/R_work/Buna/srtm/srtm_44_11.tif"
+r <- raster(r1)
+plot(r)
+plot(gadm36_ETH_2_sp, add = TRUE)
+
+#crop down the DEM raster and border .shpfile
+
+#Woredas
+gadm36_ETH_2_sp <- readRDS("C:/Users/rkopp/Desktop/R_work/Buna/gadm36_ETH_2_sp.rds")
+keff_cropped <- crop(x = gadm36_ETH_2_sp ,
+                       extent(35.42, 36.80, 6.22, 8.10))
+plot(keff_cropped)
+
+
+r_cropped <- crop(x = r ,
+                  extent(35.42, 36.80, 6.22, 8.10))
+
+
+par(mar = c(0, 0, 0, 0))
+plot(r_cropped)
+plot(keff_cropped, add = TRUE)
+
+#Kabeles--cropping to zoom down to mainly decha
+gadm36_ETH_3_sp <- readRDS("C:/Users/rkopp/Desktop/R_work/Buna/gadm36_ETH_3_sp.rds")
+
+Dech_crop <- crop(x = gadm36_ETH_3_sp ,
+                  extent(35.49, 36.5, 6.22, 7.56))
+
+par(mar = c(0, 0, 0, 0))
+plot(Dech_crop)
+
+#Interviews aka "farmers"
+
+farmers <- st_read("C:/Users/rkopp/Desktop/R_work/Cost_Distance_R/Decha_Transect_Points/Decha_Transect_Points.shp")
+
+plot(farmers, col = "black", add = TRUE)
+
+
+
+
+
+#roads <- st_read("C:/Users/rkopp/Desktop/R_work/Buna/ethiopia-latest-free_OS/gis_osm_roads_free_1.shp")
+
+#plot(roads, add = TRUE)
+
+#-----------------------------------------------------------------------------------
+
+#calculate the altitudinal differences between cells.
+
+altDiff <- function(x){x[2] - x[1]}
+hd <- transition(r, altDiff, 8, symm=FALSE)
+
+
+#Then use the geoCorrection function to divide by the distance between cells.
+slope <- geoCorrection(hd)
